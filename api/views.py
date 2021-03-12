2806f53f1a009452d104b02fbd44a60ed41b1c44
e-kondr01 <e.kondr01@gmail.com>
2021-03-12 17:01:43 +0300

    deploying on heroku for demo

--- api/views.py
+++ api/views.py
@@ -4,10 +4,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework import status as drf_status
 from rest_framework.views import APIView
-from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.response import Response
-
-from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from .models import *
 from .serializers import *
